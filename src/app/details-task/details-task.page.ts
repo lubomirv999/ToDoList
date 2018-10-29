7a196620125b05574b58c1b4d0a812b5d49c7dbb
Lubo <lubomir999@abv.bg>
2018-10-29 16:13:24 +0200

    Functionality Completed
    Functionality Completed

--- src/app/details-task/details-task.page.ts
+++ src/app/details-task/details-task.page.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { TasksList } from '../models/task-list.model';
 import { TasksService } from '../services/tasks-service';
 import { ActivatedRoute, Router } from '@angular/router';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-details-task',
@@ -12,7 +13,7 @@ export class DetailsTaskPage implements OnInit {
   task: TasksList;
   id: string;
 
-  constructor(private taskService: TasksService, private route: ActivatedRoute, private router: Router) { }
+  constructor(private alertController: AlertController, private taskService: TasksService, private route: ActivatedRoute, private router: Router) { }
 
   ngOnInit() {
     this.id = this.route.snapshot.params['id'];
@@ -23,9 +24,25 @@ export class DetailsTaskPage implements OnInit {
   }
 
   delete(id: string) {
-    this.taskService.deleteCar(id)
-      .subscribe((data) => {
-        this.router.navigateByUrl('/');
-      })
+    let alert = this.alertController.create({
+      header: 'Delete Task?',
+      message: 'Are you sure you want to delete the task?',
+      buttons: [
+        {
+          text: 'Cancel',
+          handler: () => {
+          }
+        },
+        {
+          text: 'Delete',
+          handler: () => {
+            this.taskService.deleteCar(id)
+              .subscribe((data) => {
+                this.router.navigateByUrl('/');
+              })
+          }
+        }
+      ]
+    }).then(a => a.present());
   }
 }
